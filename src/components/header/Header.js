246eb9e6c025f4aadf3ea87aa754d2f15c5d0e4c
Ali ODABAŞI <aliodabas_a@hotmail.com>
2023-02-19 15:16:47 +0300

    input type changed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,7 +10,7 @@ const Header = ({ searchQuerry, setSearchQuerry }) => {
         <input
           onChange={(e) => setSearchQuerry(e.target.value)}
           value={searchQuerry}
-          type="text"
+          type="search"
           placeholder="Search player..."
         />
       </form>
